896e24ca87e3988845cb21eb44527204f3cf8d39
He Fei <droxer.he@gmail.com>
2015-11-29 22:22:39 +0800

    renaming.

--- observable.go
+++ observable.go
@@ -6,8 +6,8 @@ type Observable struct {
 	on OnSubscribe
 }
 
-func Create(fn OnSubscribe) *Observable {
-	return &Observable{fn}
+func Create(on OnSubscribe) *Observable {
+	return &Observable{on}
 }
 
 func (o *Observable) Subscribe(sub Subscriber) {
